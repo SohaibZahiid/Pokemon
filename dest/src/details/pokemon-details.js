8f9e5210f3aabeb02d0df6fecc9c1dd52fee9583
Sohaib Zahid <sohaib642@gmail.com>
2023-07-03 15:57:35 +0200

    Finished the pokemon project

--- dest/src/details/pokemon-details.js
+++ dest/src/details/pokemon-details.js
@@ -14,10 +14,13 @@ const heightEl = document.querySelector('.height');
 const weightEl = document.querySelector('.weight');
 const params = new URLSearchParams(window.location.search);
 const pokemonId = Number(params.get("id"));
+const bodyEl = document.querySelector('body');
 function getPokemon(id) {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const pokemon = yield response.json();
+        const backgroundColor = getColor(pokemon.types[0].type.name) || '';
+        bodyEl.style.backgroundColor = backgroundColor;
         if (nameEl)
             nameEl.innerHTML = pokemon.name;
         if (imageEl)
@@ -31,4 +34,36 @@ function getPokemon(id) {
     });
 }
 getPokemon(pokemonId);
+//Gets Specific Color Based On Pokemon Type
+function getColor(type) {
+    let backgroundColor = '';
+    if (type == 'fire') {
+        backgroundColor = "#e72324";
+    }
+    else if (type == 'grass') {
+        backgroundColor = "#3da224";
+    }
+    else if (type == 'water') {
+        backgroundColor = "#2481ef";
+    }
+    else if (type == 'bug') {
+        backgroundColor = "#92a212";
+    }
+    else if (type == 'normal') {
+        backgroundColor = "#a0a2a0";
+    }
+    else if (type == 'poison') {
+        backgroundColor = "#923fcc";
+    }
+    else if (type == 'electric') {
+        backgroundColor = "#f9c000";
+    }
+    else if (type == 'ground') {
+        backgroundColor = "#92501b";
+    }
+    else if (type == 'fairy') {
+        backgroundColor = "#ef70ef";
+    }
+    return backgroundColor;
+}
 export {};
--- src/details/pokemon-details.ts
+++ src/details/pokemon-details.ts
@@ -9,9 +9,15 @@ const weightEl: HTMLElement | null = document.querySelector('.weight')
 const params = new URLSearchParams(window.location.search);
 const pokemonId: number = Number(params.get("id"));
 
+const bodyEl = document.querySelector('body')
+
 async function getPokemon(id: number) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     const pokemon = await response.json()
+
+    const backgroundColor: string = getColor(pokemon.types[0].type.name) || ''
+
+    bodyEl!.style.backgroundColor = backgroundColor
 
     if (nameEl) nameEl.innerHTML = pokemon.name;
     if (imageEl) imageEl.src = pokemon.sprites.front_default;
@@ -22,3 +28,30 @@ async function getPokemon(id: number) {
 }
 
 getPokemon(pokemonId)
+
+//Gets Specific Color Based On Pokemon Type
+function getColor(type: string) {
+    let backgroundColor: string = ''
+
+    if (type == 'fire') {
+        backgroundColor = "#e72324"
+    } else if (type == 'grass') {
+        backgroundColor = "#3da224"
+    } else if (type == 'water') {
+        backgroundColor = "#2481ef"
+    } else if (type == 'bug') {
+        backgroundColor = "#92a212"
+    } else if (type == 'normal') {
+        backgroundColor = "#a0a2a0"
+    } else if (type == 'poison') {
+        backgroundColor = "#923fcc"
+    } else if (type == 'electric') {
+        backgroundColor = "#f9c000"
+    } else if (type == 'ground') {
+        backgroundColor = "#92501b"
+    } else if (type == 'fairy') {
+        backgroundColor = "#ef70ef"
+    }
+
+    return backgroundColor
+}
